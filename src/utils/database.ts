a84736fcd07ad55ee258232959b4f5ee165ec5f0
Zander Picon <zander@zanderp25.com>
2024-09-29 15:28:33 -0400

    fix postgres url

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -2,7 +2,7 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
 import postgres from "postgres";
 
-const dbPath = `postgres://${process.env["PG_USER"]}:${process.env["PG_PASSWORD"]}@localhost:${process.env["PG_PORT"]}/${process.env["PG_DATABASE"]}`;
+const dbPath = `postgres://${process.env["PG_USER"]}:${process.env["PG_PASSWORD"]}@${process.env["PG_HOST"]}:${process.env["PG_PORT"]}/${process.env["PG_DATABASE"]}`;
 
 const migrationClient = postgres(dbPath, { max: 1 });
 await migrate(drizzle(migrationClient), { migrationsFolder: "./drizzle" });
